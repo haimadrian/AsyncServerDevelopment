a1e857719d00ab3f193772b018c30f9da6d7bdc2
roisu <roisusi@gmail.com>
2021-09-20 11:01:34 +0300

    First Commit

---
+++ Project/Code/expense-management-app/src/view/home/tabs/statistic/statistics.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../../profile/profile.css"
+
+
+export default function Statistics() {
+    return (<div className="logo">
+        <i><img src={"/logo192.png"} alt={"Logo"}/></i>
+        <span> Sign in </span>
+    </div>);
+}
