f25e5d5e6c27e2b7b11b698e1ef6fc50166d8845
“ShayM” <shay1mualem@gmail.com>
2021-09-21 17:00:32 +0300

    - change to  new Date

--- Project/Code/expense-statistics/routes/stats.js
+++ Project/Code/expense-statistics/routes/stats.js
@@ -41,57 +41,77 @@ router.post('/fetch', auth, (req, res) => { // todo add auth, // req.userId
 
     console.log(`GET yearly statistics for a user year: ${year}, month: ${month}`);
 
-    if (year && month) { // get daily Expenses
+    /*  monthValue : 
+    A zero-based integer representing the month of the year offset from the start of the year.
+    So, 0 represents January, 11 represents December,
+    -1 represents December of the previous year,
+    and 12 represents January of the following year.
+  */
 
-        dailyExpenses.find({
-            userId: 'user-Id'
-            , date: {
-                $gte: ("$" + year + "-" + "$" + month + "-02"),
-                //$gte: ("2000-10-01"),
-                //$lt: ("2000-11-01"),
-                $lt: ("$" + year + "-" + "$" + (month + 1) + "-02")
-            }
-        }
-            , { _id: 0, __v: 0 })
-            .sort({ "date": 1 })
-            .exec()
-            .then(expense_statistics => res.status(200).json(expense_statistics))
-            .catch(error => res.status(500).json({ message: error.message }));
+    /* dayValue : 0 is provided for dayValue, the date will be set to the last day of the previous month. */
 
-        /*  dailyExpenses.aggregate([
-             { $addFields: { "month": { $month: '$date' } } },
-             { $addFields: { "year": { $year: '$date' } } },
-             { $match: { month: month, year: year } }
-         ])
-             .sort({ "date": 1 })
-             .exec()
-             .then(expense_statistics => res.status(200).json(expense_statistics))
-             .catch(error => res.status(500).json({ message: error.message })); */
+    // create new date 
+    const startTime = new Date();
+    if (year) startTime.setYear(year);
+    if (month) startTime.setMonth(month - 1); else startTime.setMonth(0);
+    startTime.setDate(1);
+    startTime.setHours(0, 0, 0, 0);
 
-    } else if (year) { // get monthly Expenses
+    const endTime = new Date();
+    if (year) endTime.setYear(year);
+    if (month) endTime.setMonth(month); else endTime.setMonth(11);
+    endTime.setDate(1);
+    endTime.setHours(0, 0, 0, -1);
 
-        monthlyExpenses.find({
-            userId: 'user-Id'
-            , date: {
-                $gte: ("$" + year + "-" + "01-01"),
-                // $gte: ("2001-01-01"),
-                // $lt: ("2001-12-31"),
-                $lt: ("$" + year + "-" + "12-31")
-            }
-        }
-            , { _id: 0, __v: 0 })
-            .sort({ "date": 1 })
-            .exec()
-            .then(expense_statistics => res.status(200).json(expense_statistics))
-            .catch(error => res.status(500).json({ message: error.message }));
+    console.log(`startTime: ${startTime}, endTime: ${endTime}`);
 
-    }  //empty
+    let collection = dailyExpenses;
+    if (!month)
+        collection = monthlyExpenses;
+    /* else  // get all */
 
+    collection.find({
+        userId: 'user-Id'
+
+        /* , date: { //! this works DKW need to start 02
+            $gte: ("$" + year + "-" + "$" + month + "-02"),
+            //$gte: ("2000-10-01"),
+            //$lt: ("2000-11-01"),
+            $lt: ("$" + year + "-" + "$" + (month + 1) + "-02")
+        } */
+
+        , date: { $gte: startTime, $lt: endTime }
+    }
+        , { _id: 0, __v: 0 })
+        .sort({ "date": -1 })
+        .exec()
+        .then(expense_statistics => res.status(200).json(expense_statistics))
+        .catch(error => res.status(500).json({ message: error.message }));
+
+    /*   if (year && month) { // get daily Expenses
+ 
+     } else if (year) { // get monthly Expenses
+ 
+         monthlyExpenses.find({
+              userId: 'user-Id'
+              , date: {
+                  $gte: ("$" + year + "-" + "01-01"),
+                  // $gte: ("2001-01-01"),
+                  // $lt: ("2001-12-31"),
+                  $lt: ("$" + year + "-" + "12-31")
+              }
+          }
+              , { _id: 0, __v: 0 })
+              .sort({ "date": 1 })
+              .exec()
+              .then(expense_statistics => res.status(200).json(expense_statistics))
+              .catch(error => res.status(500).json({ message: error.message }));
+   
+     }  //empty
+ */
 
 
 });
-
-
 
 
 module.exports = router;
@@ -99,3 +119,14 @@ module.exports = router;
 
 /*  $gte: ("$" + year + "-" + "01"),
  $lt: ("$" + year + "-" + "$" + month) */
+
+
+/*  dailyExpenses.aggregate([
+            { $addFields: { "month": { $month: '$date' } } },
+            { $addFields: { "year": { $year: '$date' } } },
+            { $match: { month: month, year: year } }
+        ])
+            .sort({ "date": 1 })
+            .exec()
+            .then(expense_statistics => res.status(200).json(expense_statistics))
+            .catch(error => res.status(500).json({ message: error.message })); */
