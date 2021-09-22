720ccb860d83c3528343f74432949a3ffa2231ea
Haim Adrian <haimadrian@gmail.com>
2021-09-22 04:36:06 +0300

    Fix stats

--- Project/Code/expense-statistics/routes/stats.js
+++ Project/Code/expense-statistics/routes/stats.js
@@ -1,117 +1,50 @@
 const express = require('express');
 const router = express.Router();
-
 const auth = require("../middleware/auth");
-const statistics = require("../model/mongo/expense_statistics");
+const dailyExpenses = require("../model/mongo/daily_expenses");
+const monthlyExpenses = require("../model/mongo/monthly_expenses");
 
-const mongoose = require('mongoose');
-const dailyExpenses = mongoose.model('dailyexpenses', statistics);
-const monthlyExpenses = mongoose.model('monthlyexpenses', statistics);
-//const weeklyExpenses = mongoose.model('weeklyexpenses', statistics);
-//const yearlyExpenses = mongoose.model('yearlyexpenses', statistics);
-
-// router.post('/fetch', auth, (req, res) => {
-router.get('/fetch/year/:year/:month?', auth, (req, res) => {
-
-    // http://localhost:3002/statistics/fetch/year/2021/  || http://localhost:3002/statistics/fetch/year/2021/10
-
-    /* const year = req.body.year;
-    const month = req.body.month; */
-    const year = req.params.year;
-    const month = req.params.month;
-
-    console.log(`year: ${year}, month: ${month}`);
-
-    /* monthValue : 
+function handleFetchRange(req, res, collection, year, month = undefined) {
+    /* monthValue :
     0 represents January, 11 represents December,
     -1 represents December of the previous year,
     and 12 represents January of the following year.
     dayValue :
     0 is provided for dayValue, the date will be set to the last day of the previous month. */
 
-    // create new dates 
+    // create new dates
     const startTime = new Date();
-    if (year) startTime.setYear(year); else startTime.setYear(0);
-    if (month) startTime.setMonth(month - 1); else startTime.setMonth(0);
-    startTime.setDate(1);
-    startTime.setHours(0, 0, 0, 0);
+    startTime.setUTCFullYear(year, month || 0, 1);
+    startTime.setUTCHours(0, 0, 0, 0);
 
-    const endTime = new Date();
-    if (year) endTime.setYear(year); else endTime.setYear(new Date().getFullYear());
-    if (month) endTime.setMonth(month); else endTime.setMonth(11);
-    endTime.setDate(1);
-    endTime.setHours(0, 0, 0, -1);
-
-    console.log(`startTime: ${startTime}, endTime: ${endTime}`);
-
-    //  According to what received from the body, refer to the appropriate collection
-    let collection = dailyExpenses;
-    if (!month)
-        collection = monthlyExpenses;
-
-    collection.find({
-        userId: req.userId // user-Id
-        , date: { $gte: startTime, $lt: endTime }
+    const endTime = new Date(startTime.getTime());
+    if (month) {
+        endTime.setUTCMonth(month + 1);
+    } else {
+        endTime.setUTCFullYear(year + 1);
     }
-        , { _id: 0, __v: 0 })
-        .sort({ "date": -1 })
+
+    console.log('startTime:', startTime, 'endTime:', endTime);
+
+    collection.find({userId: req.userId, date: {$gte: startTime, $lt: endTime}},
+        {_id: 0, __v: 0})
+        .sort({"date": 1})
         .exec()
         .then(expense_statistics => res.status(200).json(expense_statistics))
-        .catch(error => res.status(500).json({ message: error.message }));
+        .catch(error => res.status(500).json({message: error.message}));
+}
 
+router.get('/fetch/year/:year', auth, (req, res) => {
+    const year = req.params.year;
+    console.log(`year: ${year}`);
+    handleFetchRange(req, res, monthlyExpenses, year);
+});
+
+router.get('/fetch/year/:year/month/:month', auth, (req, res) => {
+    const year = parseInt(req.params.year);
+    const month = parseInt(req.params.month);
+    console.log('year:', year, 'month:', month);
+    handleFetchRange(req, res, dailyExpenses, year, month);
 });
 
 module.exports = router;
-
-/* ----------------------------------- // ----------------------------------- */
-
-/*  $gte: ("$" + year + "-" + "01"),
- $lt: ("$" + year + "-" + "$" + month) */
-
-
-/*  dailyExpenses.aggregate([
-            { $addFields: { "month": { $month: '$date' } } },
-            { $addFields: { "year": { $year: '$date' } } },
-            { $match: { month: month, year: year } }
-        ])
-            .sort({ "date": 1 })
-            .exec()
-            .then(expense_statistics => res.status(200).json(expense_statistics))
-            .catch(error => res.status(500).json({ message: error.message })); */
-
-
-
-/* ----------------------------------- // ----------------------------------- */
-
-
-/* , date: { //! this works DKW need to start 02
-    $gte: ("$" + year + "-" + "$" + month + "-02"),
-    //$gte: ("2000-10-01"),
-    //$lt: ("2000-11-01"),
-    $lt: ("$" + year + "-" + "$" + (month + 1) + "-02")
-} */
-
-/* ----------------------------------- // ----------------------------------- */
-
-
-/*   if (year && month) { // get daily Expenses
-
-     } else if (year) { // get monthly Expenses
-
-         monthlyExpenses.find({
-              userId: 'user-Id'
-              , date: {
-                  $gte: ("$" + year + "-" + "01-01"),
-                  // $gte: ("2001-01-01"),
-                  // $lt: ("2001-12-31"),
-                  $lt: ("$" + year + "-" + "12-31")
-              }
-          }
-              , { _id: 0, __v: 0 })
-              .sort({ "date": 1 })
-              .exec()
-              .then(expense_statistics => res.status(200).json(expense_statistics))
-              .catch(error => res.status(500).json({ message: error.message }));
-
-     }  //empty
- */
