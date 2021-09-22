c3a297ea8570d2fbfe69831d85ddf7a28409c3dc
roisu <roisusi@gmail.com>
2021-09-22 13:18:18 +0300

    first commit for DB

--- Project/Code/expense-management-app/src/view/home/tabs/statistic/statistics.js
+++ Project/Code/expense-management-app/src/view/home/tabs/statistic/statistics.js
@@ -4,10 +4,34 @@ import React, {useCallback, useState} from "react";
 import PieChartStatistics from "./PieChartStatistic/PieChartStatistics";
 import Card from "../manage/components/UI/Card";
 import FilterDates from "./Options/ComboBoxList"
+import axios from "axios";
+import urls from "../../../../model/backend_url";
+import {potentiallyRefreshToken} from "../../../../firebase";
+import {get} from "firebase/firebase-database";
 
 export default function statistic() {
     //Todo take data from DB
     //1. category and amount
+
+    const httpErrorHandler = useCallback(async (error) => {
+        let errorMessage = error.response?.data?.message;
+        if (!errorMessage) {
+            errorMessage = error.toString();
+        }
+        console.error(errorMessage);
+        return potentiallyRefreshToken(error);
+    }, []);
+
+
+    function getData(date) {
+        axios.post(urls.getStatsMonthly(date))
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch(httpErrorHandler);
+    }
+
+    getData(2021);
 
     return (
         <div className='frame-profile'>
